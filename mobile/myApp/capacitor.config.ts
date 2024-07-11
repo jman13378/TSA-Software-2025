9259b3e9ba30ef94ca00402c6c69832edc462c2a
jman13378 <garconiamail@gmail.com>
2024-07-10 20:50:49 -0400

    e

--- mobile/myApp/capacitor.config.ts
+++ mobile/myApp/capacitor.config.ts
@@ -3,7 +3,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'org.chaptercore.app',
   appName: 'ChapterCore',
-  webDir: 'dist'
+  webDir: 'dist',
+  
 };
 
 export default config;
