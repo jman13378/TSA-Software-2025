5bce5db2fa3eccce4c07cd4905c58e5fc6ded940
jman13378 <garconiamail@gmail.com>
2024-08-13 19:00:01 -0400

    e

--- gitupdator/testing.js
+++ gitupdator/testing.js
@@ -1,25 +1,37 @@
+const simpleGit = require("simple-git");
 const { exec } = require("child_process");
 const path = require("path");
 
 // Define the repository details
 const repoURL = "https://jman13378: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@github.com/PBMHS-TSA/TSA-Software-2025.git";
-const repoPath = path.join(__dirname, "root", "TSA-Software-2025");
+const repoPath = path.join(__dirname, "..");
+const git = simpleGit(repoPath);
+
+// Function to set the remote URL with credentials
+async function setRemoteUrl() {
+  try {
+    await git.removeRemote('origin');
+    await git.addRemote('origin', repoURL);
+  } catch (err) {
+    console.error(`Error setting remote URL: ${err.message}`);
+  }
+}
 
 // Function to pull from Git and run commands
-function updateRepo() {
-  console.log("Pulling the latest changes from Git...");
+async function updateRepo() {
+  try {
+    console.log("Pulling the latest changes from Git...");
 
-  // Command to pull from the Git repository
-  exec(`git -C ${repoPath} pull ${repoURL}`, (err, stdout, stderr) => {
-    if (err) {
-      console.error(`Error pulling from Git: ${stderr}`);
-      return;
-    }
+    // Ensure the remote URL with credentials is set
+    await setRemoteUrl();
 
-    console.log(stdout);
+    // Pull the latest changes from the Git repository
+    await git.pull("origin", "main");
+
+    console.log("Pulled the latest changes.");
 
     // Change directory and run npm install
-    exec(`cd ${repoPath} && npm i`, (err, stdout, stderr) => {
+    exec(`npm i`, { cwd: repoPath }, (err, stdout, stderr) => {
       if (err) {
         console.error(`Error running npm install: ${stderr}`);
         return;
@@ -28,7 +40,7 @@ function updateRepo() {
       console.log(stdout);
 
       // Run nodemon
-      exec(`cd ${repoPath} && nodemon index.ts`, (err, stdout, stderr) => {
+      exec(`nodemon index.ts`, { cwd: repoPath }, (err, stdout, stderr) => {
         if (err) {
           console.error(`Error running nodemon: ${stderr}`);
           return;
@@ -37,7 +49,9 @@ function updateRepo() {
         console.log(stdout);
       });
     });
-  });
+  } catch (err) {
+    console.error(`Error during Git operation: ${err.message}`);
+  }
 }
 
 // Set the interval to pull from Git every 30 seconds
